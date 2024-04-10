59068bdca2a635ca6d4f93e786763f5465bca9e2
GizmoLD <122391569+GizmoLD@users.noreply.github.com>
2024-04-10 20:09:37 +0200

    .

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 
 // settings
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 
 // middlewares
 app.use(express.json());
