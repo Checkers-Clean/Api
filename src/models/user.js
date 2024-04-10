42958601b9ec586bf0e566e64a7ac011ce33e508
GizmoLD <122391569+GizmoLD@users.noreply.github.com>
2024-04-10 19:08:27 +0200

    .

---
+++ src/models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre("save", function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const document = this;
+    bcrypt.hash(document.password, saltRounds, (err, hashedPassword) => {
+      if (err) {
+        next(err);
+      } else {
+        document.password = hashedPassword;
+        next();
+      }
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.isCorrectPassword = function (password, callback) {
+  bcrypt.compare(password, this.password, (err, same) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(err, same);
+    }
+  });
+};
+
+module.exports = mongoose.model("User", userSchema);
---
+++ src/routes/user.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const userSchema = require("../models/user");
+const jwt = require('jsonwebtoken');
+
+const router = express.Router();
+
+// create user
+router.post("/users", (req, res) => {
+  const user = userSchema(req.body);
+  user
+    .save()
+    .then((data) => res.status(200).json(data)) 
+    .catch((error) => res.status(500).json({ message: error })); 
+});
+
+// login user
+router.post("/authenticate", (req, res) => {
+  const { email, password } = req.body;
+
+  // Buscar el usuario por su correo electrónico
+  userSchema.findOne({ email }, (err, user) => {
+    if (err) {
+      return res.status(500).json({ message: "Internal server error" });
+    }
+    if (!user) {
+      return res.status(401).json({ message: "Incorrect email" });
+    }
+    // Verificar si la contraseña proporcionada es correcta
+    user.isCorrectPassword(password, (err, same) => {
+      if (err) {
+        return res.status(500).json({ message: "Internal server error" });
+      }
+      if (!same) {
+        return res.status(401).json({ message: "Incorrect password" });
+      }
+      // Si la contraseña es correcta, enviar respuesta de éxito
+      res.status(200).json({ message: "Logged in successfully" });
+    });
+  });
+});
+/////////////////////////////////////////////////////////////////////////////////////////
+// get all users
+router.get("/users", (req, res) => {
+  userSchema
+    .find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// get a user
+router.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  userSchema
+    .findById(id)
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// delete a user
+router.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  userSchema
+    .remove({ _id: id })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// update a user
+router.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, email, password } = req.body;
+  userSchema
+    .updateOne({ _id: id }, { $set: { name, email, password } })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+module.exports = router;
